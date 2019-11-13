e9a9260e3778738e8830f17fc34d0289b81bb505
Sujaira <susi141@gmail.com>
2019-11-13 14:31:21 -0500

    using api to get movie titles on home page

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const request = require("request");
 const bodyParser = require("body-parser");
+var bestMovies = [];
+var movies = [];
+var upcomingMovies = [];
+var tvShows = [];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,7 +14,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    var url = "http://www.omdbapi.com/?s=harry+potter&apikey=fc1d3669";
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            bestMovies = data["Search"];
+            res.render("index", { bestMovies: bestMovies });
+        }
+    });
+});
+
+app.get("/:p/s", (req, res) => {
+    var page = req.params.p;
+    console.log(typeof(page));
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query +
+        "&apikey=fc1d3669"
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            bestMovies = data["Search"];
+            res.render(page, { bestMovies: bestMovies });
+        }
+    });
+});
+
+app.get("/upcoming", (req, res) => {
+    res.render("upcoming");
+});
+
+app.get("/movies", (req, res) => {
+    res.render("movies");
+});
+
+app.get("/tvshows", (req, res) => {
+    res.render("tvshows");
 });
 
 app.listen(3000, () => {

28292beea2b82a6c1971517927556ac72e036d2a
Sujaira <susi141@gmail.com>
2019-11-17 11:37:03 -0500

    adding movies and tv shows logic

--- app.js
+++ app.js
@@ -26,10 +26,21 @@ app.get("/", (req, res) => {
 
 app.get("/:p/s", (req, res) => {
     var page = req.params.p;
-    console.log(typeof(page));
     var query = req.query.search;
-    var url = "http://www.omdbapi.com/?s=" + query +
-        "&apikey=fc1d3669"
+    var url = "http://www.omdbapi.com/?s=";
+    if (page === "tvshows") {
+        console.log("This comes from Tv Shows");
+        url = url + query +
+            "&type=series&apikey=fc1d3669";
+    } else if (page === "movies") {
+        console.log("This comes from Movies");
+        url = url + query +
+            "&type=movie&apikey=fc1d3669";
+    } else {
+        console.log("This comes from Index");
+        url = url + query +
+            "&apikey=fc1d3669";
+    }
     request(url, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
@@ -39,23 +50,26 @@ app.get("/:p/s", (req, res) => {
     });
 });
 
-app.get("/upcoming", (req, res) => {
-    var url = "http://www.omdbapi.com/?s=harry+potter&apikey=fc1d3669";
+app.get("/movies", (req, res) => {
+    var url = "http://www.omdbapi.com/?s=star+wars&type=movie&apikey=fc1d3669";
     request(url, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
-            upcomingMovies = data["Search"];
-            res.render("upcoming", { bestMovies: upcomingMovies });
+            bestMovies = data["Search"];
+            res.render("movies", { bestMovies: bestMovies });
         }
     });
 });
 
-app.get("/movies", (req, res) => {
-    res.render("movies");
-});
-
 app.get("/tvshows", (req, res) => {
-    res.render("tvshows");
+    var url = "http://www.omdbapi.com/?s=one+tree+hill&type=series&apikey=fc1d3669";
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            bestMovies = data["Search"];
+            res.render("tvshows", { bestMovies: bestMovies });
+        }
+    });
 });
 
 app.listen(3000, () => {
